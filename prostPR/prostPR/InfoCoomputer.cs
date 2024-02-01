0e5d84af4d6cd90b8beba4cb408d20f22154276a
Фирсов Вадим Леонидович <144768438+m4wid@users.noreply.github.com>
2024-02-01 11:56:21 +0300

    Add files via upload

---
+++ prostPR/prostPR/InfoCoomputer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+using System.Threading.Tasks;
+
+namespace prostPR
+{
+    public class InfoCoomputer
+    {
+        public static List<string> GetHardwareInfo(string WIN32_Class, string ClassItemField)
+        {
+            List<string> result = new List<string>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + WIN32_Class);
+            switch (ClassItemField)
+            {
+                case "Capacity":
+                    int Capacity = 0;
+                    foreach (ManagementObject m in searcher.Get()) Capacity += Convert.ToInt32(Math.Round(Convert.ToDouble(m[ClassItemField]) / 1024 / 1024));
+                    result.Add(Capacity.ToString() + " Мб");
+                    break;
+                default:
+                    foreach (ManagementObject obj in searcher.Get()) result.Add(obj[ClassItemField].ToString().Trim());
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ prostPR/prostPR/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prostPR
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void versionbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show($"{InfoCoomputer.GetHardwareInfo("Win32_Processor", "Name")[0]}\n{InfoCoomputer.GetHardwareInfo("Win32_VideoController", "Name")[0]}\n{InfoCoomputer.GetHardwareInfo("Win32_DiskDrive", "Caption")[0]}", "Конфигурация ПК", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
